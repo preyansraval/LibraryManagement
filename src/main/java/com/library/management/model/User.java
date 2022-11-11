52e8d1f4f0d3480268bf1f979cb5b4486b35efbe
Dhvanan Trivedi <dhvanan.trivedi@gmail.com>
2022-11-11 14:35:49 +0530

    added fields to User class

--- src/main/java/com/library/management/model/User.java
+++ src/main/java/com/library/management/model/User.java
@@ -1,4 +1,36 @@
 package com.library.management.model;
 
 public class User {
+	
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 }
